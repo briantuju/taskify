16929727aa34ad918453247a6c6b91ed4ff5d1f8
Brian Tuju <briantuju@gmail.com>
2021-03-17 16:09:54 +0300

    feat: Add utility helper
    This helper contains helper methods and classes that enforces the
    DRY principle.

---
+++ src/utils/helpers.js
@@ -0,0 +1,128 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+// Extend dayjs
+dayjs.extend(customParseFormat);
+dayjs.extend(relativeTime);
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+// Guess the timezone and set it as default timezone
+dayjs.tz.setDefault(dayjs.tz.guess());
+
+/**
+ * Helper class to work with date and time
+ */
+export class DateTime {
+  /**
+   * Return relative time from now
+   * @param {String} date
+   */
+  static fromNow(date) {
+    return dayjs(date).fromNow();
+  }
+
+  /**
+   * Converts an `ISO date string` to `ISO date` and `ISO time` strings
+   * @param {String} isoString
+   */
+  static toIsoDateTime(isoString) {
+    const indexOfT = isoString.indexOf("T");
+    const indexOfPeriod = isoString.indexOf(".");
+
+    return {
+      date: isoString.substring(0, indexOfT),
+      time: isoString.substring(indexOfT + 1, indexOfPeriod),
+    };
+  }
+}
+
+/**
+ * Helper class to access the browser's storage
+ * @access public
+ */
+export class Storage {
+  /**
+   * Save item to local storage
+   * @param {String} key
+   * @param {*} value
+   * @static @public
+   */
+  static setItem(key, value) {
+    return localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  /**
+   * Save auth token to local storage
+   * @param {String} token
+   * @static @public
+   */
+  static setJwtToken(token) {
+    return this.setItem("jwt", token);
+  }
+
+  /**
+   * Retrieve an item from local storage
+   * @param {String} key
+   * @static @public @returns {?String}
+   */
+  static getItem(key) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+
+  /**
+   * Retrieve auth token
+   * @static @public @returns {?String}
+   */
+  static getAuthToken() {
+    return this.getItem("jwt");
+  }
+
+  /**
+   * Remove an item from local storage
+   * @param {String} key
+   * @static @public
+   */
+  static removeItem(key) {
+    return localStorage.removeItem(key);
+  }
+
+  /**
+   * Remove auth token from storage
+   * @static @public
+   */
+  static removeAuthToken() {
+    return this.removeItem("jwt");
+  }
+}
+
+/**
+ * Check if environment is browser or node
+ */
+export const isBrowser = () => typeof window !== "undefined";
+
+/**
+ * Extract form values from form element
+ * @param {HTMLFormElement} e
+ * @returns {object}
+ */
+export const extractFormData = (e) => {
+  let data = {};
+
+  const formData = new FormData(e.target);
+  for (const item of formData.entries()) data[item[0]] = item[1];
+
+  return data;
+};
+
+/**
+ * Redirect page to the specified location with full page reload
+ * @param {String} location
+ * @returns {void}
+ */
+export const hardRedirectLocation = (location) => {
+  window.location = location;
+};
