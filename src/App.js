622a0723073b63668733bc06065e05c2fc8ef390
Brian Tuju <briantuju@gmail.com>
2021-03-17 17:55:39 +0300

    update: Update the app to consume context and auth states
    We use context to globally store the auth state.
    Authorization to resources is also restricted using the
    AuthRoutes component.
    Placeholder ppp pages have also been updated.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
+import { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import { pageUrls } from "./utils/constants";
+import { AuthContext } from "./context/auth";
 import Layout from "./components/Layout";
-import Landing from "./pages/Landing";
-import NotFound from "./pages/NotFound";
+import AuthRoutes from "./AuthRoutes";
+import * as pages from "./pages";
 import "./App.css";
+import { AppStorage } from "./utils/helpers";
 
 function App() {
-  return (
-    <Layout>
-      <Switch>
-        <Route exact path={pageUrls.landing}>
-          <Landing />
-        </Route>
+  // Get token and save to state
+  const token = AppStorage.getAuthToken();
+  const [authToken, setAuthToken] = useState(token);
 
-        <Route path={pageUrls.notFound}>
-          <NotFound />
-        </Route>
-      </Switch>
-    </Layout>
+  // Save the token
+  const setTokens = (token) => {
+    // Save token to Storage
+    AppStorage.setJwtToken(token);
+
+    // Save token to state
+    setAuthToken(token);
+  };
+
+  return (
+    <AuthContext.Provider value={{ authToken, setAuthToken: setTokens }}>
+      <Layout>
+        <Switch>
+          <Route exact path={pageUrls.landing}>
+            <pages.Landing />
+          </Route>
+
+          <Route path={pageUrls.signup}>
+            <pages.Signup />
+          </Route>
+
+          <Route path={pageUrls.login}>
+            <pages.Login />
+          </Route>
+
+          <Route path={pageUrls.activateAccount}>
+            <pages.ActivateAccount />
+          </Route>
+
+          <Route path={pageUrls.forgotPassword}>
+            <pages.ForgotPassword />
+          </Route>
+
+          <Route path={pageUrls.resetPassword}>
+            <pages.ResetPassword />
+          </Route>
+
+          <Route path={pageUrls.productPlans}>
+            <pages.ProductPlans />
+          </Route>
+
+          <AuthRoutes path={pageUrls.account}>
+            <pages.Account />
+          </AuthRoutes>
+
+          <AuthRoutes path={pageUrls.home}>
+            <pages.Home />
+          </AuthRoutes>
+
+          <AuthRoutes path={`${pageUrls.board}/:id`}>
+            <pages.Board />
+          </AuthRoutes>
+
+          <AuthRoutes path={`${pageUrls.task}/:id`}>
+            <pages.Task />
+          </AuthRoutes>
+
+          <Route path={pageUrls.notFound}>
+            <pages.NotFound />
+          </Route>
+        </Switch>
+      </Layout>
+    </AuthContext.Provider>
   );
 }
 
