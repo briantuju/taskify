0547c67b2f5db8929e07a788deb3aca83faf7862
Brian Tuju <briantuju@gmail.com>
2021-03-22 14:26:17 +0300

    feat: Create a show hide component

---
+++ src/components/ShowHide.js
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+
+const ShowHide = ({ isExpanded, handleToggle, text }) => {
+  // Handle setting the expanded state to true or false
+  const toggleIsExpanded = () => {
+    if (isExpanded === true) return handleToggle(false);
+    return handleToggle(true);
+  };
+
+  return (
+    <span
+      className={"d--i-block brad bg--light m--y-tiny click--cursor"}
+      onClick={toggleIsExpanded}
+    >
+      <small className="m--x-tiny">
+        {isExpanded ? `Hide ${text ? text : ""}` : `Show ${text ? text : ""}`}
+      </small>
+    </span>
+  );
+};
+
+ShowHide.propTypes = {
+  isExpanded: PropTypes.bool.isRequired,
+  handleToggle: PropTypes.func.isRequired,
+};
+
+export default ShowHide;
