e12b6402b7846a678c09e6fb22561dddadcf6c31
Brian Tuju <briantuju@gmail.com>
2021-03-22 11:58:47 +0300

    feat: Initialize account page

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,5 +1,27 @@
+import { useAuth } from "../context/auth";
+import { endpoints } from "../utils/constants";
+import { Overlay } from "../components/Loading";
+import Activate from "../components/account/Activate";
+import useApi from "../hooks/useApi";
+
 const Account = () => {
-  return <h1>Account</h1>;
+  // Get auth state
+  const { setAuthToken } = useAuth();
+
+  // Fetch account data
+  const [{ loading, data, error, msg }] = useApi(
+    endpoints.account.get + "?unified=true"
+  );
+
+  if (loading) return <Overlay />;
+
+  return (
+    <div className="container p--y-1">
+      <h1>Welcome to your account page</h1>
+
+      {data && !data.data.user.verification.isVerified && <Activate />}
+    </div>
+  );
 };
 
 export default Account;
