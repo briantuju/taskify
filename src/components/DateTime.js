d8893164f65a71c83e9e7178e325b5054f6bbcdb
Brian Tuju <briantuju@gmail.com>
2021-03-26 22:46:00 +0300

    update: Update component props

--- src/components/DateTime.js
+++ src/components/DateTime.js
@@ -4,7 +4,12 @@ import DatePicker from "react-datepicker";
 // We MUST bring in the CSS too
 import "react-datepicker/dist/react-datepicker.css";
 
-export const InlineDateTime = ({ startDate, onDateChange, ...props }) => {
+export const InlineDateTime = ({
+  startDate,
+  onDateChange,
+  className,
+  ...props
+}) => {
   return (
     <DatePicker
       selected={startDate}
@@ -12,6 +17,7 @@ export const InlineDateTime = ({ startDate, onDateChange, ...props }) => {
       dateFormat="MM/dd/yyyy h:mm"
       inline
       showTimeInput
+      className={`${className ? className : ""}`}
       {...props}
     />
   );
